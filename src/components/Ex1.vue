946ef5c986baf0974af0bab831858457638a6ab0
QuekKangSheng <ks.quek.2023@smu.edu.sg>
2025-09-24 01:36:36 +0000

    done for ex1

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,18 +1,37 @@
 <script>
     export default { 
-        // Add Code Here
-        // challenge: use array  
+        data() {
+            return {
+                msg : "Choose your favorite fruit: ",
+                fruit1: "apple",
+                fruit2: "orange",
+                fruit3: "peach",
+
+                /*using array of fruit*/
+                fruits:["apple","orange","peach"]
+            }
+        }
     }
 </script>
 
 <template>
-    <div id="app">
-        {{ message }} </br>
-        {{ fruit1 }} <input type="radio" name="fruit">
-        {{ fruit2 }} <input type="radio" name="fruit">
-        {{ fruit3 }} <input type="radio" name="fruit">
-    </div>
+  <div id="app">
+    {{ msg }}
+    {{ fruit1 }} <input type="radio" name="fruit">
+    {{ fruit2 }} <input type="radio" name="fruit">
+    {{ fruit3 }} <input type="radio" name="fruit">
+    <p>
+        <h3>Using an array</h3>
+        {{ msg }}
+        <span v-for="fruit in fruits">
+            {{ fruit }} <input type="radio" name="fruit">
+        </span>
+    </p>
+  </div>
 </template>
 
 <style scoped>
+.greeting {
+    color: red;
+}
 </style>
